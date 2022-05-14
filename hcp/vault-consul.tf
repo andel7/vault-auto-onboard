cbd09b6675f493c8038eb879f2d339fa4864869d
Lev Andelman <levandelman@Cheburashka.lan>
2022-05-14 12:35:50 +0300

    i hate pycharm

--- hcp/vault-consul.tf
+++ hcp/vault-consul.tf
@@ -6,7 +6,7 @@ resource "hcp_vault_cluster" "demo_hcp_vault" {
 
 resource "hcp_vault_cluster_admin_token" "demo_hcp_vault_token" {
   cluster_id = hcp_vault_cluster.demo_hcp_vault.cluster_id
-}git
+}
 
 resource "hcp_consul_cluster" "demo_hcp_consul" {
   hvn_id          = hcp_hvn.demo_hcp_hvn.hvn_id
